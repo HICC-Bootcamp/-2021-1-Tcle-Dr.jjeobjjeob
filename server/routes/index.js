15fe822db801dafdaecafe9388fa5672effa1e56
u-nij <u.nij0509@gmail.com>
2021-08-21 15:46:15 +0900

    회원 가입 버튼 생성

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', isAuth, function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/auth/login');
 });
 
 export default router;
